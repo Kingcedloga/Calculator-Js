31571e55e337328acbad58b210264cc03ee06734
Meced Loga <mecedloga@gmail.com>
2023-08-11 04:16:41 +0100

    Retrieve DOM elements

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 { calculate } import './calculator';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
+
+// Récupérer les éléments du DOM
+const inputElement = document.getElementById('input');
+const calculElement = document.getElementById('calcul');
+const buttons = document.querySelectorAll('.numpad, #plusoumoins, #percentage');
+const resetButton = document.getElementById('reset');
+const clearButton = document.getElementById('clear');
+const equalsButton = document.getElementById('equals');
+const doubleZeroButton = document.getElementById('double-zero');
