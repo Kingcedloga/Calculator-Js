8cceb92049608dc7df3b3da2cd188946a5b59c82
Meced Loga <mecedloga@gmail.com>
2023-08-13 16:22:59 +0100

    Refactor

--- script.js
+++ script.js
@@ -10,49 +10,56 @@ const resetButton = document.getElementById('reset');
 const clearButton = document.getElementById('clear');
 const equalsButton = document.getElementById('equals');
 const doubleZeroButton = document.getElementById('double-zero');
+
 // Ajouter des écouteurs d'événements aux boutons
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
-      addToInput(button.textContent);
-    });
+  button.addEventListener('click', () => {
+    addToInput(button.textContent);
   });
-  resetButton.addEventListener('click', () => {
-    resetCalculator();
-  });
-  clearButton.addEventListener('click', () => {
-    clearInput();
-  });
-  equalsButton.addEventListener('click', () => {
-    calculateResult();
-  });
-  
+});
+
+resetButton.addEventListener('click', () => {
+  resetCalculator();
+});
+
+clearButton.addEventListener('click', () => {
+  clearInput();
+});
+
+equalsButton.addEventListener('click', () => {
+  calculateResult();
+});
+
 doubleZeroButton.addEventListener('click', () => {
-    addToInput('00');
-  });
-  // Fonction pour ajouter du texte à l'entrée
+  addToInput('00');
+});
+
+// Fonction pour ajouter du texte à l'entrée
 function addToInput(value) {
-  inputElement.value += value;
+  // Vérifier si le point est déjà présent dans l'entrée
+  if (value === '.' && inputElement.value.includes('.')) {
+    return;
+  }
+
+  // Vérifier si l'entrée commence par zéro
+  if (inputElement.value === '0' && value !== '.') {
+    inputElement.value = value;
+  } else {
+    inputElement.value += value;
+  }
 }
-// // Vérifier si le point est déjà présent dans l'entrée
-if (value === '.' && inputElement.value.includes('.')) {
-  return;
-}
-// Vérifier si l'entrée commence par zéro
-if (inputElement.value === '0' && value !== '.') {
-  inputElement.value = value;
-} else {
-  inputElement.value += value;
-}
+
 // Fonction pour réinitialiser la calculatrice
-function resetCalculator ()  {
+function resetCalculator() {
   inputElement.value = '';
   calculElement.textContent = '';
 }
 
 // Fonction pour effacer l'entrée
-function clearInput () {
+function clearInput() {
   inputElement.value = '';
 }
+
 // Fonction pour calculer le résultat
 function calculateResult() {
   const input = inputElement.value;
@@ -62,9 +69,16 @@ function calculateResult() {
   if (input.endsWith('.')) {
     calculation += '0';
   }
-//   // Vérifier si l'entrée est une opération valide
-  if (isValidOperation(input)) {
-    const result = eval(calculation);
+
+  // Vérifier si l'opération est valide
+  if (isValidOperation(calculation)) {
+    let result = eval(calculation);
+
+    // Gérer le pourcentage
+    if (calculElement.textContent === '') {
+      result /= 100;
+    }
+
     inputElement.value = result;
     calculElement.textContent = calculation + ' =';
   } else {
@@ -74,20 +88,8 @@ function calculateResult() {
   }
 }
 
-  // Gérer le pourcentage
-  if (calculElement.textContent === '') {
-    result /= 100;
-  }
-
-//   inputElement.value = result;
-//   calculElement.textContent = calculation + ' =';
-// } else {
-//   inputElement.value = '';
-//   calculElement.textContent = '';
-//   alert('Opération invalide !');
-// }
 // Fonction pour vérifier si l'opération est valide
 function isValidOperation(input) {
-const regex = /^\s*-?\d+(\.\d+)?\s*([-+*/]\s*-?\d+(\.\d+)?\s*)*$/;
-return regex.test(input);
+  const regex = /^\s*-?\d+(\.\d+)?\s*([-+*/]\s*-?\d+(\.\d+)?\s*)*$/;
+  return regex.test(input);
 }
